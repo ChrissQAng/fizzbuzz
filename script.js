147e8681d72ed575f67811b799f1cb16a20eb12b
ChrissQAng <132443487+ChrissQAng@users.noreply.github.com>
2023-06-28 11:24:53 +0200

    java script allgorithms are ready - output only in the console

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-let a = "Hello World!"
-alert(a)
+let answer = parseInt(prompt("Please enter the number you would like to fizzbuzz up to:", "number"));
+
+for (let i=1; i <= answer; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("Fizzbuzz")
+    }
+    else if (i % 3 === 0) {
+        console.log("Fizz")
+    }
+    else if (i % 5 === 0) {
+        console.log("Buzz")
+    }
+    
+
+    else {
+    console.log(i);
+         }
+        }
